d2449e8218049b2754857070ab3b70450a94dbb2
omar20alaa <omar302ao@gmail.com>
2022-06-04 16:46:10 +0200

    first commit

---
+++ app/src/main/java/app/jetpack/MainActivity.kt
@@ -0,0 +1,30 @@
+package app.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.jetpack.ui.theme.HomeScreen
+import app.jetpack.ui.theme.MeditationUiJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+
+
